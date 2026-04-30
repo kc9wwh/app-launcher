9ceafbc911d497bdd71f2c01b542bf3de4989f00
Josh Roskos <josh@kc9wwh.net>
2026-04-29 20:22:11 -0500

    style: widen cards by reducing max columns to 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
 		</div>
 	{/if}
 
-	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 		{#each data.apps as app}
 			<AppCard {app} />
 		{:else}
